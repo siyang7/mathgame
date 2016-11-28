fa8f4c7eabc2d1d3c94ae2d89c0dd74aa11a4a95
Si-Yang <siyang7@hotmail.com>
2016-11-28 13:10:08 -0800

    change

--- authenticate.php
+++ authenticate.php
@@ -24,9 +24,9 @@
             
             
             <div class="row">
-                <label class="col-sm-2 col-sm-offset-3">    <?php  $val1 = echo(rand(0,20)); ?>  </label>
+                <label class="col-sm-2 col-sm-offset-3">    <?php  echo(rand(0,20)); ?>  </label>
                 <label class="col-sm-2">+</label>
-                <label class="col-sm-2">    <?php   $val2 = echo(rand(0,20)); ?>  </label>
+                <label class="col-sm-2">    <?php   echo(rand(0,20)); ?>  </label>
                 <div class="col-sm-3"></div>
             </div>
             
