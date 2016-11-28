0934b02efb8360844bbbe45e5016fc27b0196443
Si-Yang <siyang7@hotmail.com>
2016-11-27 23:05:16 -0800

    Updates

--- header.php
+++ header.php
@@ -3,7 +3,8 @@
 
 <head>
 	<title>Math Game</title>
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+    <link href="styles/styles.css" rel="stylesheet" />
   <meta charset="utf-8" />
 </head>
 <body>
