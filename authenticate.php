ab911616f81c739784fde4a8dd8c8023ecea14d3
Si-Yang <siyang7@hotmail.com>
2016-11-27 23:48:31 -0800

    changes

--- authenticate.php
+++ authenticate.php
@@ -11,49 +11,50 @@
 </head>
 <body>
     <div class="container">
-<form action="index.php" method="post" role="form" class="form-horizontal">
-    <div class="row">
-        <div class="col-sm-4 col-sm-offset-4"><h1>Math Game</h1></div>
-        <div class="col-sm-4"><a href="logout.php" class="btn btn-default btn-sm">Logout</a></div>
-    </div>
-    <div class="row">
-        <label class="col-sm-2 col-sm-offset-3">6</label>
-        <label class="col-sm-2">+</label>
-        <label class="col-sm-2">4</label>
-        <div class="col-sm-3"></div>
-    </div>
-
-    <input type="hidden" name="first_number" value="6" />
-    <input type="hidden" name="operation" value="+" />
-    <input type="hidden" name="second_number" value="4" />
-    <input type="hidden" name="total" value="0" />
-    <input type="hidden" name="score" value="0" />
-
-    <div class="form-group">
-        <div class="col-sm-3 col-sm-offset-4">
-            <input type="text" class="form-control" id="answer" name="answer" placeholder="Enter answer" size="6">
-        </div>
-        <div class="col-sm-5"></div>
-    </div>
-    <div class="row">
-        <div class="col-sm-3 col-sm-offset-4">
-            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <button type="submit" class="btn btn-primary btn-sm">
-            Submit</button>
-        </div>
-        <div class="col-sm-3"></div>
-    </div>
-</form>
-<hr />
-<div class="row">
-    <div class="col-sm-4 col-sm-offset-4">
+        <form action="index.php" method="post" role="form" class="form-horizontal">
+            <div class="row">
+                <div class="col-sm-4 col-sm-offset-4"><h1>Math Game</h1></div>
+                <div class="col-sm-4"><a href="logout.php" class="btn btn-default btn-sm">Logout</a></div>
             </div>
-    <div class="col-sm-4"></div>
-</div>
-<div class="row">
-    <div class="col-sm-4 col-sm-offset-4">Score: 0 / 0</div>
-    <div class="col-sm-4"></div>
-</div>
+            <div class="row">
+                <label class="col-sm-2 col-sm-offset-3">6</label>
+                <label class="col-sm-2">+</label>
+                <label class="col-sm-2">4</label>
+                <div class="col-sm-3"></div>
+            </div>
+
+            <input type="hidden" name="first_number" value="6" />
+            <input type="hidden" name="operation" value="+" />
+            <input type="hidden" name="second_number" value="4" />
+            <input type="hidden" name="total" value="0" />
+            <input type="hidden" name="score" value="0" />
+
+            <div class="form-group">
+                <div class="col-sm-3 col-sm-offset-4">
+                    <input type="text" class="form-control" id="answer" name="answer" placeholder="Enter answer" size="6">
+                </div>
+                <div class="col-sm-5"></div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3 col-sm-offset-4">
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button type="submit" class="btn btn-primary btn-sm">
+                    Submit</button>
+                </div>
+                <div class="col-sm-3"></div>
+            </div>
+        </form>
+        
+        <hr />
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4">
+            </div>
+            <div class="col-sm-4"></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4">Score: 0 / 0</div>
+            <div class="col-sm-4"></div>
+        </div>
     </div>
 </body>
 </html>
