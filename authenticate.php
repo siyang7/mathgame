9635adfe18f40949c1dce5b56e3785ab675e8702
Si-Yang <siyang7@hotmail.com>
2016-11-27 23:38:58 -0800

    changes

--- authenticate.php
+++ authenticate.php
@@ -6,6 +6,7 @@
 <head>
 	<title>Math Game</title>
     <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="styles/styles.css" rel="stylesheet" />
   <meta charset="utf-8" />
 </head>
 <body>
