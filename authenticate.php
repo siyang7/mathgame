1b93c8ba000c2cf706d90b0fbf45c23bb9b8eebc
Si-Yang <siyang7@hotmail.com>
2016-11-28 12:23:23 -0800

    changes

--- authenticate.php
+++ authenticate.php
@@ -1,7 +1,7 @@
 <?php session_start(); 
 
-$val1 = (rand(0,20));
-$val2 = (rand(0,20));
+//$val1 = (rand(0,20));
+//$val2 = (rand(0,20));
 
 ?>
 
@@ -24,9 +24,9 @@ $val2 = (rand(0,20));
             
             
             <div class="row">
-                <label class="col-sm-2 col-sm-offset-3">    <?php  $val1; ?>  </label>
+                <label class="col-sm-2 col-sm-offset-3">    <?php  (rand(0,20)); ?>  </label>
                 <label class="col-sm-2">+</label>
-                <label class="col-sm-2">    <?php $val2; ?>  </label>
+                <label class="col-sm-2">    (rand(0,20)); ?>  </label>
                 <div class="col-sm-3"></div>
             </div>
             
