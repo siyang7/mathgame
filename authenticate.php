c58c313031fce63034c4a5b92dd88130d27be1a6
Si-Yang <siyang7@hotmail.com>
2016-11-28 23:26:25 -0800

    f

--- authenticate.php
+++ authenticate.php
@@ -138,7 +138,7 @@ if ($_POST['answer'] == $_POST['correctAnswer']) {
        $_SESSION['countCorrect'] = $_SESSION['countCorrect'] + 1;
        echo "<div class='col-sm-4'>Correct.</div>";
        
-   } else {
+   } else if ($_SESSION['countCorrect'] != $_SESSION['countCorrect'] + 1) {
        
        //$_SESSION["countCorrect"] = 0;
        echo "<div class='col-sm-4'>Incorrect."   . $var1 . "&nbsp;" . $operator . "&nbsp;" . $var2 . "&nbsp;" . "is " . $answer . "</div>";
