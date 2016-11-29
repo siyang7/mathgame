cd4289aee625b09f3f00b821ba8821782b070dad
Si-Yang <siyang7@hotmail.com>
2016-11-29 00:36:28 -0800

    f

--- index.php
+++ index.php
@@ -11,8 +11,8 @@
                 $_SESSION['login'] = true; header('LOCATION: authenticate.php'); die();
         }
           
-        if($username !== 'a@a.a')$userError = 'Invalid Username';
-        if($password !== 'aaa')$passError = 'Invalid Password';
+        if($username !== 'a@a.a')$userError = '<p style=\"color: red\">Invalid Username</p>';
+        if($password !== 'aaa')$passError = '<p style=\"color: red\">Invalid Password</p>';
         
         }
 
