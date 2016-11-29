ce3c12c72908b0c4c632249e2f68fa8fc9bb2bfb
Si-Yang <siyang7@hotmail.com>
2016-11-28 23:05:28 -0800

    f

--- authenticate.php
+++ authenticate.php
@@ -130,8 +130,8 @@ echo "<!DOCTYPE HTML>
 </html>";
 
 
-/*
-if ($_POST["answer"] == $_POST["correctAnswer"]) {
+
+if ($_POST["$answer"] == $_POST["correctAnswer"]) {
     
    if (isset($_SESSION['countCorrect'])) {
        
@@ -143,7 +143,7 @@ if ($_POST["answer"] == $_POST["correctAnswer"]) {
        //$_SESSION["countCorrect"] = 0;
        echo "<div class='col-sm-4'>Incorrect.</div>";
    }  
-*/
+
 
 
 /*
