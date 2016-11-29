a3bd6d6bed98c80444fd76b7cd07e56b4157afb9
Si-Yang <siyang7@hotmail.com>
2016-11-28 23:48:41 -0800

    f

--- authenticate.php
+++ authenticate.php
@@ -20,11 +20,11 @@ if (isset($_SESSION['count'])) {
     $_SESSION['count'] = $_SESSION['count'] + 1;
 } else {
     
-    echo "count = " . $_SESSION['count'];
-    echo "overall = " . $_SESSION['attempts'];
+    //echo "count = " . $_SESSION['count'];
+    //echo "overall = " . $_SESSION['attempts'];
     
     $_SESSION['count'] = 0;
-    //$_SESSION['countCorrect'] = 0;
+    $_SESSION['countCorrect'] = 0;
 }
 
 
