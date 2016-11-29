8c5e4998eabf173d7fc481683e318466ce85602b
Si-Yang <siyang7@hotmail.com>
2016-11-28 16:19:40 -0800

    change

--- authenticate.php
+++ authenticate.php
@@ -1,70 +1,70 @@
 <?php session_start(); 
 
 
+//echo(rand(0,20));
 
-?>
 
-<!DOCTYPE HTML>
-<html lang="en">
+echo "<!DOCTYPE HTML>
+<html lang='en'>
 
 <head>
 	<title>Math Game</title>
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" media="screen">
-    <link href="styles/styles.css" rel="stylesheet" />
-  <meta charset="utf-8" />
+    <link href='http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css' rel='stylesheet' media='screen'>
+    <link href='styles/styles.css' rel='stylesheet' />
+  <meta charset='utf-8' />
 </head>
 <body>
-    <div class="container">
-        <form action="index.php" method="post" role="form" class="form-horizontal">
-            <div class="row">
-                <div class="col-sm-4 col-sm-offset-4"><h1>Math Game</h1></div>
-                <div class="col-sm-4"><a href="index.php" class="btn btn-default btn-sm">Logout</a></div>
+    <div class='container'>
+        <form action='index.php' method='post' role='form' class='form-horizontal'>
+            <div class='row'>
+                <div class='col-sm-4 col-sm-offset-4'><h1>Math Game</h1></div>
+                <div class='col-sm-4'><a href='index.php' class='btn btn-default btn-sm'>Logout</a></div>
             </div>
             
             
-            <div class="row">
-                <label class="col-sm-2 col-sm-offset-3">    <?php  echo(rand(0,20)); ?>  </label>
-                <label class="col-sm-2">+</label>
-                <label class="col-sm-2">    <?php   echo(rand(0,20)); ?>  </label>
-                <div class="col-sm-3"></div>
+            <div class='row'>
+                <label class='col-sm-2 col-sm-offset-3'>    6  </label>
+                <label class='col-sm-2'>+</label>
+                <label class='col-sm-2'>    4  </label>
+                <div class='col-sm-3'></div>
             </div>
             
             
 
-            <input type="hidden" name="first_number" value="6" />
-            <input type="hidden" name="operation" value="+" />
-            <input type="hidden" name="second_number" value="4" />
-            <input type="hidden" name="total" value="0" />
-            <input type="hidden" name="score" value="0" />
+            <input type='hidden' name='first_number' value='6' />
+            <input type='hidden' name='operation' value='+' />
+            <input type='hidden' name='second_number' value='4' />
+            <input type='hidden' name='total' value='0' />
+            <input type='hidden' name='score' value='0' />
 
-            <div class="form-group">
-                <div class="col-sm-3 col-sm-offset-4">
-                    <input type="text" class="form-control" id="answer" name="answer" placeholder="Enter answer" size="6">
+            <div class='form-group'>
+                <div class='col-sm-3 col-sm-offset-4'>
+                    <input type='text' class='form-control' id='answer' name='answer' placeholder='Enter answer' size='6'>
                 </div>
-                <div class="col-sm-5"></div>
+                <div class='col-sm-5'></div>
             </div>
-            <div class="row">
-                <div class="col-sm-3 col-sm-offset-4">
+            <div class='row'>
+                <div class='col-sm-3 col-sm-offset-4'>
                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <button type="submit" class="btn btn-primary btn-sm">
+                    <button type='submit' class='btn btn-primary btn-sm'>
                     Submit</button>
                 </div>
-                <div class="col-sm-3"></div>
+                <div class='col-sm-3'></div>
             </div>
         </form>
         
         <hr />
-        <div class="row">
-            <div class="col-sm-4 col-sm-offset-4">
+        <div class='row'>
+            <div class='col-sm-4 col-sm-offset-4'>
             </div>
-            <div class="col-sm-4"></div>
+            <div class='col-sm-4'></div>
         </div>
-        <div class="row">
-            <div class="col-sm-4 col-sm-offset-4">Score: 0 / 0</div>
-            <div class="col-sm-4"></div>
+        <div class='row'>
+            <div class='col-sm-4 col-sm-offset-4'>Score: 0 / 0</div>
+            <div class='col-sm-4'></div>
         </div>
     </div>
 </body>
-</html>
+</html>";
 
-<?php ?>
+?>
--- index.php
+++ index.php
@@ -20,13 +20,13 @@
 echo "<!DOCTYPE HTML>
     
     
-<html lang="en">
+<html lang='en'>
 
 <head>
 	<title>Math Game</title>
-    <link href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" media="screen" />
-    <link href="styles/styles.css" rel="stylesheet" />
-  <meta charset="utf-8" />
+    <link href='http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css' rel='stylesheet' media='screen' />
+    <link href='styles/styles.css' rel='stylesheet' />
+  <meta charset='utf-8' />
 </head>
 
     <body>
